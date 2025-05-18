beb43bfeb9b6df0bfdad31242768133e7555b992
DavidRodriguezCardona <davidrocar1311@gmail.com>
2025-05-18 17:42:43 -0500

    graficos

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+function useFetch<T>(url: string) {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) throw new Error("Error al cargar datos");
+        return res.json();
+      })
+      .then((json) => {
+        console.log("Datos recibidos:", json);
+        setData(json);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, [url]);
+
+  return { data, loading, error };
+}
+
+export default useFetch; 
+
