5f46c9e80fe379b702ba02b148f663d440a2880e
Redo08 <juandavidreyesortiz8@gmail.com>
2025-05-18 22:38:18 -0500

    Funcionamiento sidebar dependiente si el usuario existe

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -58,7 +58,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
   }, [sidebarExpanded]);
 
   return (
-    <div> 
+    <div> {user ? 
     <aside
       ref={sidebar}
       className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${
@@ -883,7 +883,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
           </div>
       </div>
     </aside>
-    :<></>
+    :<div></div>}
     </div>
   );
 };
